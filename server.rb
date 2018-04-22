03851e17c8eae65d06d8f44c0df9c9cae9938fe4
Karl Meinkopf <kremen.karl@yandex.ru>
2018-04-22 19:53:09 +0600

    initial

---
+++ server.rb
@@ -0,0 +1,28 @@
+require 'sinatra/base'
+
+class Server < Sinatra::Base
+
+	def append(file, string)
+		IO.write file, [
+			(IO.read(file) || ''),
+			string
+		].join("\n").strip
+	end
+
+	before do
+		content_type 'text/plain'
+	end
+
+	get '/task' do
+		'ping'
+	end
+
+	get '/ping/:id' do
+		append 'pings', params[:id] unless params[:id].strip == 'favicon.ico'
+		status 200
+	end
+
+	get '/favicon.ico' do
+		status 200
+	end
+end
